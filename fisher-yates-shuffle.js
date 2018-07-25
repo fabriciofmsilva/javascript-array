d31bff7d8c2fbd73419f80d0641f7e32c7e79431
Fabricio Francisco Mendes da Silva | S2IT <fabricio.silva@S2IT.CORP>
2018-07-25 10:58:11 -0300

    Reduce code

--- fisher-yates-shuffle.js
+++ fisher-yates-shuffle.js
@@ -1,10 +1,11 @@
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 function shuffle(list) {
-  for (let index = list.length; index; index--) {
-    const randomIndex = Math.floor(Math.random() * index);
-    const element = list[index - 1];
-    [list[index - 1], list[randomIndex]] = [list[randomIndex], list[index - 1]];
+  let index = list.length;
+
+  while (index) {
+    const randomIndex = Math.floor(Math.random() * index--);
+    [list[index], list[randomIndex]] = [list[randomIndex], list[index]];
   }
 }
 
